de4efc101c9e6aaa5fcea1037bf257ed24a88d3d
Mateus <mateus.silvafreitas27@gmail.com>
2024-03-08 23:23:00 -0300

    criando invocação de método com parametro

--- WebFramework/src/main/java/br/com/msfreitas/webframework/web/WebFrameworkDispatcherServlet.java
+++ WebFramework/src/main/java/br/com/msfreitas/webframework/web/WebFrameworkDispatcherServlet.java
@@ -1,8 +1,11 @@
 package br.com.msfreitas.webframework.web;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
 
 import com.google.gson.Gson;
 
@@ -23,7 +26,7 @@ public class WebFrameworkDispatcherServlet extends HttpServlet {
 		// ignorar favicon
 		if (req.getRequestURL().toString().endsWith("/favicon.ico"))
 			return;
-		
+
 		PrintWriter out = new PrintWriter(resp.getWriter());
 		Gson gson = new Gson();
 
@@ -35,33 +38,69 @@ public class WebFrameworkDispatcherServlet extends HttpServlet {
 
 		WebFrameworkLogger.log("WebFrameworkDispatcherServlet", "URL: " + url + "(" + httpMethod + ") - Handler "
 				+ data.getControllerClass() + "." + data.getControllerMethod());
-		
-		//verificar se existir um instancia da classe correpondente, caso não, criar uma 
+
+		// verificar se existir um instancia da classe correpondente, caso não, criar
+		// uma
 		Object controller;
 		WebFrameworkLogger.log("WebFrameworkDispatcherServlet", "Procurar instancia da controladora");
 		try {
 			controller = ControllerInstances.instace.get(data.controllerClass);
-			if(controller == null) {
+			if (controller == null) {
 				WebFrameworkLogger.log("WebFrameworkDispatcherServlet", "Criando uma nova instancia da controladora");
 				controller = Class.forName(data.controllerClass).getDeclaredConstructor().newInstance();
 				ControllerInstances.instace.put(data.controllerClass, controller);
 			}
-			
-			//precisamos extrair o método desta classe - ou seja o método que vai atender a requisição. Vamos executar esse método e escrever a saida dele.
+
+			// precisamos extrair o método desta classe - ou seja o método que vai atender a
+			// requisição. Vamos executar esse método e escrever a saida dele.
 			Method controllerMethod = null;
-			for(Method method : controller.getClass().getMethods()) {
-				if(method.getName().equals(data.getControllerMethod())) {
+			for (Method method : controller.getClass().getMethods()) {
+				if (method.getName().equals(data.getControllerMethod())) {
 					controllerMethod = method;
 					break;
 				}
 			}
+
+			if (controllerMethod.getParameterCount() > 0) {
+				WebFrameworkLogger.log("WebFrameworkDispatcherServlet",
+						"Método " + controllerMethod.getName() + " tem parâmetros!");
+				Object arg;
+				Parameter parameter = controllerMethod.getParameters()[0];
+				if (parameter.getAnnotations()[0].annotationType().getName()
+						.equals("br.com.msfreitas.webframework.annotations.WebFrameworkBody")) {
+
+					WebFrameworkLogger.log("",
+							"     Procurando parâmetro da requisição do tipo " + parameter.getType().getName());
+					String body = readBytesFromRequest(req);
+
+					WebFrameworkLogger.log("", "     conteúdo do parâmetro: " + body);
+					arg = gson.fromJson(body, parameter.getType());
+
+					WebFrameworkLogger.log("WebFrameworkDispatcherServlet",
+							"Invocar o método " + controllerMethod.getName() + " com o parâmetro do tipo "
+									+ parameter.getType().toString() + " para requisição");
+					out.println(gson.toJson(controllerMethod.invoke(controller, arg)));
+				}
+			}else {
+				WebFrameworkLogger.log("WebFrameworkDispatcherServlet",
+						"Invocar o método " + controllerMethod.getName() + " para requisição");
+
+				out.println(gson.toJson(controllerMethod.invoke(controller)));
+			}
 			
-			WebFrameworkLogger.log("WebFrameworkDispatcherServlet", "Invocar o método " + controllerMethod.getName() + " para requisição");
-			
-			out.println(gson.toJson(controllerMethod.invoke(controller)));
 			out.close();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+	
+	private String readBytesFromRequest(HttpServletRequest req) throws Exception {
+		StringBuilder stringBuilder = new StringBuilder();
+		String line;
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(req.getInputStream()));
+		while((line = bufferedReader.readLine()) != null) {
+			stringBuilder.append(line);
+		}
+		return stringBuilder.toString();
 	}
 }
